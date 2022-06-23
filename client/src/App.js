7ddb0f8d09169ab75d971c27d951f075bd2c3214
sheryar <royalsheryar505@gmail.com>
2022-06-23 19:00:12 +0500

    Done creating registration/login form and integrate it with backend (Closes #4)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import React from 'react'
 import Todo from "./components/Todo";
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
 function App() {
+  let navigate = useNavigate();
+
+  React.useEffect(() => {
+
+    const isLoggedIn = window.localStorage.getItem('token') ? true : false;
+    isLoggedIn ? navigate('/') : navigate('/signIn');
+    
+  }, [window.localStorage.getItem('token')])
+
   return (
-    <Todo />
+    <Routes>
+      <Route path="/" element={<Todo />} /> :
+      <Route exact path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
   );
 }
 
